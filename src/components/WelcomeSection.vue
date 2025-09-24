b73a541364c72273f5ae6b9815d00d0098ebbd16
parol <info@parolin.dev>
2025-09-24 15:46:28 +0200

    Add welcome section

---
+++ src/components/WelcomeSection.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import SmilingEmoji from "../assets/emojis/smiling.png";
+import LinkedInButton from "./common/LinkedInButton.vue";
+import GitHubButton from "./common/GitHubButton.vue";
+import FullstackDevCard from "./welcome/FullstackDevCard.vue";
+import MobileDevCard from "./welcome/MobileDevCard.vue";
+</script>
+
+<template>
+  <section class="flex w-full flex-col items-center px-6 md:px-8 lg:max-w-6xl">
+    <div class="mt-6 mb-16 flex flex-col items-center justify-center">
+      <img :src="SmilingEmoji" class="mb-4 h-32" alt="Matteo Parolin emoji" />
+
+      <h1 class="text-center text-2xl font-bold tracking-tighter text-white">
+        Matteo Parolin
+      </h1>
+      <p class="text-center text-sm font-medium tracking-tight text-white/75">
+        Freelance software engineer
+      </p>
+
+      <div class="mt-2 flex gap-2">
+        <LinkedInButton />
+        <GitHubButton />
+      </div>
+    </div>
+
+    <div
+      class="flex w-full max-w-[31em] flex-col items-center justify-center gap-16 lg:max-w-full lg:flex-row lg:gap-12 xl:gap-16"
+    >
+      <FullstackDevCard class="lg:h-80" />
+      <MobileDevCard class="lg:h-80" />
+    </div>
+  </section>
+</template>
---
+++ src/components/welcome/FullstackDevCard.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import Globe from "./LightedGlobe.vue";
+import Laravel from "../../assets/technologies/laravel.svg";
+import VueJS from "../../assets/technologies/vuejs.svg";
+import Tailwind from "../../assets/technologies/tailwind.svg";
+</script>
+
+<template>
+  <div
+    class="flex h-full w-full flex-col gap-4 overflow-hidden text-white lg:h-80 lg:justify-between lg:gap-0"
+  >
+    <div class="w-full">
+      <h2 class="text-center font-medium">Full-stack web development</h2>
+      <p class="text-center text-sm text-white/75">
+        Web development services delivering unique front-end experiences
+        combined with robust back-end architecture, all tailored to boost user
+        engagement and support business objectives.
+      </p>
+    </div>
+
+    <div class="relative flex h-52 w-full justify-center">
+      <div class="absolute -bottom-18 flex w-full justify-center">
+        <Globe class="h-70 rotate-12" aria-hidden="true" />
+      </div>
+
+      <div
+        class="absolute bottom-1 z-20 flex items-center gap-1 font-mono text-xs tracking-tight text-white/60 uppercase"
+      >
+        <img :src="Laravel" class="mb-1 h-3 grayscale" aria-hidden="true" />
+        Laravel <span aria-hidden="true">+</span>
+        <img :src="VueJS" class="mb-1 h-3 grayscale" aria-hidden="true" />
+        Vue.js <span aria-hidden="true">+</span>
+        <img :src="Tailwind" class="mb-1 h-4 grayscale" aria-hidden="true" />
+        Tailwind
+      </div>
+
+      <div
+        class="absolute bottom-0 left-0 z-10 h-20 w-full bg-gradient-to-b from-transparent to-neutral-950"
+      ></div>
+    </div>
+  </div>
+</template>
