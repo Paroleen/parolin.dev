e05422a24b18ab6005a93736b0b32ffced718405
parol <info@parolin.dev>
2025-09-24 15:44:40 +0200

    Add navbar and footer

---
+++ src/components/layout/Footer.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import ExternalLinkIcon from "../icons/ExternalLinkIcon.vue";
+import LinkedInButton from "../common/LinkedInButton.vue";
+import GitHubButton from "../common/GitHubButton.vue";
+</script>
+
+<template>
+  <footer class="mx-auto w-full max-w-6xl px-6 py-4 md:px-8">
+    <div class="flex flex-col items-center justify-between md:flex-row">
+      <div
+        class="mb-6 flex flex-col items-center justify-center gap-1 text-sm text-white/75 md:mb-0 md:flex-row"
+      >
+        <span>Matteo Parolin</span>
+        <span class="hidden md:block" aria-hidden="true">|</span>
+        <span>VAT Number: IT04344770245</span>
+        <span class="hidden md:block" aria-hidden="true">|</span>
+        <a
+          href="https://github.com/Paroleen/parolin.dev"
+          class="flex items-start gap-0.5 font-medium text-white"
+        >
+          Built with Vue.js
+          <ExternalLinkIcon class="h-2.5 w-2.5" aria-hidden="true" />
+        </a>
+      </div>
+
+      <div class="flex justify-around gap-1">
+        <LinkedInButton />
+        <GitHubButton />
+      </div>
+    </div>
+  </footer>
+</template>
---
+++ src/components/layout/Navbar.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import LaptopFace from "../../assets/emojis/laptop.png";
+import LinkedInButton from "../common/LinkedInButton.vue";
+import GitHubButton from "../common/GitHubButton.vue";
+</script>
+
+<template>
+  <nav
+    class="mx-2 flex h-12 w-96 animate-navbar items-center justify-between rounded-full bg-neutral-900/70 px-4 shadow-[inset_1px_0_#ffffff0a,_inset_-1px_0_#ffffff0a,_inset_0_1px_#ffffff1a,_inset_0_1px_#ffffff1a] backdrop-blur-sm"
+  >
+    <div class="flex items-center gap-1">
+      <img
+        class="h-8 w-8"
+        :src="LaptopFace"
+        alt="Matteo Parolin emoji typing on a laptop"
+      />
+
+      <div class="flex flex-col text-white">
+        <h1 class="text-sm font-medium tracking-tight">Matteo Parolin</h1>
+        <h2 class="text-xs font-light">Freelance software engineer</h2>
+      </div>
+    </div>
+
+    <div class="flex items-center gap-1">
+      <LinkedInButton />
+      <GitHubButton />
+    </div>
+  </nav>
+</template>
